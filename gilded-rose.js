27ba942cbc4d308880d9020b3e09dae3ac46480a
Joseph Carrillo <jlcarrillojr81@gmail.com>
2023-07-07 07:12:45 -1000

    Update

--- gilded-rose.js
+++ gilded-rose.js
@@ -4,64 +4,112 @@ export class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  //Decreasing quality of item by one if the quality is above 0
+  decreaseQuality(){
+      if(this.quality > 0){
+          this.quality--
+      }
+  }
+  //Quality increase by one but never over 50
+  increaseQuality() {
+    if(this.quality < 50) {
+      this.quality++;
+    }
+  }  
+}
+
+export class Basic extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality)
+  }
+  updateQuality(){
+    this.decreaseQuality()
+    this.sellIn = this.sellIn - 1;
+    if (this.sellIn < 0) {
+      this.decreaseQuality();
+    }
+  }
+}
+
+ export class Concert extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality)
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality
+  }
+    concertOver(){
+      if(this.sellIn < 0){
+        this.quality = this.quality - this.quality
+      }
+    }
+  increaseBackStageQuality(){
+    if (this.name == "Backstage passes to a TAFKAL80ETC concert") {
+      if (this.sellIn < 11) {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1;
+        }
+      }
+      if (this.sellIn < 6) {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1;
+        }
+      }
+    }
+  }
+  updateQuality() {
+    this.increaseBackStageQuality();
+    this.increaseQuality(); 
+    this.sellIn = this.sellIn - 1;
+    this.concertOver();
+  }
+}
+
+export class Cheese extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality)
+  }
+  updateQuality(){
+    this.increaseQuality()
+    this.sellIn = this.sellIn - 1;
+  }
+}
+
+export class Legendary extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality)
+  }
+  updateQuality() {
+  }
+}
+
+export class Conjure extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality)
+  }
+  updateQuality(){
+    this.decreaseQuality()
+    this.sellIn = this.sellIn - 1;
+    if (this.sellIn < 0) {
+      this.decreaseQuality();
+    }
+    this.decreaseQuality();
+  }
 }
 
 export let items = [];
 
-items.push(new Item("+5 Dexterity Vest", 10, 20));
-items.push(new Item("Aged Brie", 2, 0));
-items.push(new Item("Elixir of the Mongoose", 5, 7));
-items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-items.push(new Item("Conjured Mana Cake", 3, 6));
+items.push(new Basic("+5 Dexterity Vest", 10, 20)); 
+items.push(new Cheese("Aged Brie", 2, 0));
+items.push(new Basic("Elixir of the Mongoose", 5, 7));
+items.push(new Legendary("Sulfuras, Hand of Ragnaros", 0, 80));
+items.push(new Concert("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+items.push(new Conjure("Conjured Mana Cake", 3, 6));
+
 
 export const updateQuality = () => {
   for (let item of items) {
-    if (
-      item.name != "Aged Brie" &&
-      item.name != "Backstage passes to a TAFKAL80ETC concert"
-    ) {
-      if (item.quality > 0) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-          item.quality = item.quality - 1;
-        }
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (item.name != "Sulfuras, Hand of Ragnaros") {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.sellIn < 0) {
-      if (item.name != "Aged Brie") {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-    }
+    item.updateQuality()
   }
 };
--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -1,9 +1,9 @@
 import { expect, describe, it } from "vitest";
-import { Item, items, updateQuality } from "./gilded-rose.js";
+import { Item, items, updateQuality, Basic, Concert, Cheese, Legendary, Conjure } from "./gilded-rose.js";
 
 describe("updateQuality", () => {
   it("reduces quality and sellIn of basic items by 1", () => {
-    const testItem = new Item("basic", 5, 3);
+    const testItem = new Basic("basic", 5, 3); //Changed to basic
     items.push(testItem);
 
     updateQuality();
@@ -11,4 +11,92 @@ describe("updateQuality", () => {
     expect(testItem.quality).toBe(2);
     expect(testItem.sellIn).toBe(4);
   });
+  it('Once the `sellIn` days is less then zero, `quality` degrades twice as fast.', ()=>{
+    const testItem = new Basic('basic', -1, 10) //Changed to basic
+    items.push(testItem)
+
+    updateQuality()
+
+    expect(testItem.quality).toBe(8);
+  });
+  it("The `quality` of an item is never negative", () => { //name, sellIn, quality
+    const testItem = new Basic('Basic', 11, 0); //Changed to basic
+    items.push(testItem);
+
+    updateQuality();
+   
+    expect(testItem.quality).toBe(0);
+  })
+  it('"Aged Brie" actually increases in `quality` the older it gets.', () => {
+    const testItem = new Cheese('Aged Brie', 10, 10)
+    items.push(testItem)
+
+    updateQuality()
+
+    expect(testItem.quality).toBe(11)
+  })
+  it('The `quality` of an item is never more than `50`.', () => {
+    const testItem = new Basic('Basic', 10, 50);
+    items.push(testItem);
+
+    updateQuality();
+
+    expect(testItem.quality).toBe(49);
+  })
+  it('Sulfuras, Hand of Ragnaros, being a legendary item, never has to be sold nor does it decrease in `quality`.', () => {
+    const testItem = new Legendary('Sulfuras, Hand of Ragnaros', 100, 100)
+    items.push(testItem)
+
+    updateQuality()
+
+    expect(testItem.quality).toBe(100)
+    expect(testItem.sellIn).toBe(100)
+  })
+  it("Backstage passes to a TAFKAL80ETC concert, increase in `quality` as it's `sellIn` value decreases:", () => {
+    const testItem1 = new Concert('Backstage passes to a TAFKAL80ETC concert', 10, 10);
+    const testItem2 = new Concert('Backstage passes to a TAFKAL80ETC concert', 5, 10);
+    const testItem3 = new Concert('Backstage passes to a TAFKAL80ETC concert', 0, 10);
+    
+    items.push(testItem1);
+    items.push(testItem2);
+    items.push(testItem3);
+
+
+    updateQuality();
+    //test item1
+    expect(testItem1.quality).toBe(12);
+    expect(testItem1.sellIn).toBe(9);
+    //test item2
+    expect(testItem2.quality).toBe(13);
+    expect(testItem2.sellIn).toBe(4);
+    //test item3
+    expect(testItem3.quality).toBe(0);
+    expect(testItem3.sellIn).toBe(-1);
+  })
+
+  it('Conjured items degrade in quality twice as fast', () => {
+    const testItem = new Conjure('Conjured Item', 10, 10);
+
+    items.push(testItem);
+
+    updateQuality();
+
+    expect(testItem.quality).toBe(8);
+  })
+  
+
 });
+
+
+/*
+XX Once the `sellIn` days is less then zero, `quality` degrades twice as fast
+XX The `quality` of an item is never negative.
+XX "Aged Brie" actually increases in `quality` the older it gets.
+XX The `quality` of an item is never more than `50`.
+XX Sulfuras, Hand of Ragnaros," being a legendary item, never has to be sold nor does it decrease in `quality`.
+XX "Backstage passes to a TAFKAL80ETC concert", increase in `quality` as it's `sellIn` value decreases:
+  - `quality` increases by `2` when there are `10` days or less left before the concert.
+  - `quality` increases by `3` when there are `5` days or less left before the concert.
+  - `quality` drops to `0` after the concert.
+XX Conjured items degrade in quality twice as fast
+*/
